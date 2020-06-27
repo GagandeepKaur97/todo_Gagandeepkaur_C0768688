341c96b5c106de16d80086dcaed038d41aff2c50
gagan <sherry2312.gk@gmail.com>
2020-06-27 17:43:45 -0400

    [gk] added new view controller

---
+++ todo_Gagandeepkaur_C0768688/MoveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoveViewController.swift
+//  todo_Gagandeepkaur_C0768688
+//
+//  Created by Gagan on 2020-06-27.
+//  Copyright © 2020 Gagan. All rights reserved.
+//
+
+import UIKit
+
+class MoveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
